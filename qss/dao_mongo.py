c4ad02019cdd8a2c13b552285ac1164d10968a4f
Otirik <120437324@qq.com>
2020-07-12 21:07:56 +0800

    Signed-off-by: Otirik <120437324@qq.com>

--- qss/dao_mongo.py
+++ qss/dao_mongo.py
@@ -1,27 +1,31 @@
 import pymongo
-
-
+import settings
 class Client:
 
-    def __init__(self, host = '', port = ''):
-        pass
+    def __init__(self, path):
+        self.mongo = pymongo.MongoClient(path)
+        self.db = self.mongo[settings.DB_NAME]
+        self.table = self.db[settings.DB_TB_NAME]
+    
+    def rget(self):#待修改
+        """get items randomly"""
+        x = self.table.find()
+        return next(x)
+    
+    def re(self,field,exp):#待修改
+        x = self.table.find({field:{"$regex":exp}})
+        return
+    
+    def insert(self,data):#待修改
+        x = self.table.insert_many(data)
+        return
 
-    def rget(self, num = 1):
-    """get items randomly"""
-        pass
+    def find(self,data):#待修改
+        x = self.table.find(data)
+        return    
+        
 
-    def re(self, exp):
-    '''返回符合的url'''
-        pass
-
-    def insert(self):
-        pass
-
-    def find(self):
-        pass
-
-    def remove(self):
-        pass
-
-    def mark(self,):
-    '''给一个id打标记'''
+    def delete(self,data):#待修改
+        x = self.table.delete_many(data)
+        print('已删除%s条数据.'%x.deleted_count)
+        return
